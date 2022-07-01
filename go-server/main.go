979c0053377ffeec19df5104b1df04e37c709ce8
alvo254 <alvinkhamron@gmail.con>
2022-07-01 21:08:19 +0300

    go server project

---
+++ go-server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandle(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprint(w, "ParseForm() err ", err)
+		return
+	}
+	fmt.Fprint(w, "Post was successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprint(w, "Name = %s\n", name)
+	fmt.Fprint(w, "Address = %s", address)
+
+}
+func helloHandle(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprint(w, "Hello")
+}
+func main() {
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+	http.HandleFunc("/form", formHandle)
+	http.HandleFunc("./hello", helloHandle)
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalln(err)
+	}
+
+}
